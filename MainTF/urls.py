0a86c332a679371475b298e699377986b9c5744e
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2021-10-03 18:32:09 +0600

    media file

--- MainTF/urls.py
+++ MainTF/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('account/', include('Account.urls')),
     path('blogs/', include('Blog.urls')),
     path('accounts/', include('allauth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
