cfb4fade88f71a4f9610a31538afda60965bf452
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2021-10-31 11:24:17 +0600

    paging problem

--- Academy/views.py
+++ Academy/views.py
@@ -86,12 +86,14 @@ def UserCoursesDetails(request, id):
 def UserFiles(request, id):
     course = models.Course.objects.get(id=id)
     # section = models.Section.objects.filter(course=course)
-    content = models.Content.objects.filter(section__course=course)
-    paginator = Paginator(content, 1)
+    contents = models.Content.objects.filter(section__course=course)
+    paginator = Paginator(contents, 1)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+
     context = {
         'course': course,
+        'contents': contents,
         'content_type': 'instruction',
         'section_no': 1,
         'module_no': 1,
