db5a5c98133fd02e8aec09116c36ee2188b89a49
mahmudsagartechforing <mahmudsagar.techforing@gmail.com>
2021-11-16 12:55:21 +0600

    main admin  navbar fix

--- static/tfjs/admin_panel/mainTF/nav.js
+++ static/tfjs/admin_panel/mainTF/nav.js
@@ -1,30 +1,47 @@
-const links = document.querySelectorAll('.nav-link')
-links.forEach(link => {
-    if (link.href == document.URL) {
-        link.classList.add("active")
-        if (link.classList.contains("dropdown-item")) {
-            console.log("clicked");
-            link.parentElement.parentElement.classList.add("show-dropdown-container");
-        }
-
-    } else {
-        link.classList.remove("active")
-    }
+const navLinks = document.querySelectorAll('.nav-link')
+navLinks.forEach(link => {
+  if (link.href == document.URL) {
+    link.classList.add("active")
+  } else {
+    link.classList.remove("active")
+  }
 })
 
+const dropdownContainers = document.querySelectorAll(".dropdown-container");
 
 const dropdown = document.querySelectorAll(".dropdown-btn");
 for (let i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function () {
-        var dropdownContent = this.nextElementSibling;
-        console.log(dropdownContent);
-        if (dropdownContent.classList.contains("show-dropdown-container")) {
-            dropdownContent.classList.remove("show-dropdown-container");
-        } else {
-            dropdownContent.classList.add("show-dropdown-container");
-        }
-    });
+  dropdown[i].addEventListener("click", function () {
+    var dropdownContent = this.nextElementSibling;
+    dropdownContainers.forEach(item=>{
+      item.classList.remove("show-dropdown-container")
+      if (item.classList.contains("active-container") && item != dropdownContent) {
+        item.classList.remove("active-container");
+        item.previousElementSibling.classList.remove("clicked")
+      }
+    })
+    if (dropdownContent.classList.contains("active-container")) {
+      dropdownContent.classList.remove("show-dropdown-container");
+      dropdownContent.classList.remove("active-container");
+      dropdown[i].classList.remove("clicked");
+    } else {
+      dropdownContent.classList.add("show-dropdown-container");
+      dropdownContent.classList.add("active-container");
+      dropdown[i].classList.add("clicked");
+    }
+  });
 }
+const mainSection = document.querySelector("main")
+mainSection.addEventListener("click", ()=>{
+  dropdownContainers.forEach(item=>{
+    item.classList.remove("show-dropdown-container")
+    item.classList.remove("active-container")
+  })
+  dropdown.forEach(item=>{
+    item.classList.remove("clicked");
+  })
+})
+
 
 const deleteBtn = document.querySelectorAll(".trash, .dlt")
 
@@ -36,8 +53,30 @@ deleteBtn.forEach(btn => {
 
 
 const hamburger = document.querySelector(".hamburger")
+const sideBarClose = document.querySelector(".sideBarClose")
+const sideBarText = document.querySelectorAll(".nav-text")
 const aside = document.querySelector(".aside-container")
+const nav = document.querySelector("nav")
+const main = document.querySelector("main")
+const footer = document.querySelector("footer")
 
 hamburger.addEventListener("click", () => {
-    aside.classList.toggle("show-aside")
+  aside.classList.toggle("show-aside")
+  aside.classList.contains("shrink-container") && aside.classList.remove("shrink-container")
+})
+
+sideBarClose.addEventListener("click", () => {
+  aside.classList.remove("show-aside")
+})
+
+const shrinkBtn = document.querySelector(".shrink-btn")
+shrinkBtn.addEventListener("click", () => {
+  aside.classList.toggle("shrink-container")
+  nav.classList.toggle("nav-expand")
+  main.classList.toggle("main-expand")
+  footer.classList.toggle("footer-expand")
+  sideBarText.forEach(text=>{
+    text.classList.toggle("d-none")
+  })
+
 })
