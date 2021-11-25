23b2d7475402d61c08f4326928542919225f34f3
salekin <salekin.techforing@gmail.com>
2021-11-25 17:56:39 +0600

    active link style fix

--- static/tfjs/user_panel/bcs/menubar.js
+++ static/tfjs/user_panel/bcs/menubar.js
@@ -1,4 +1,4 @@
-let menuItemsName = ['dashboard', 'services', 'quotation', 'order', 'my_team', 'subscriptions', 'events', 'notifications', 'support'];
+let menuItemsName = ['dashboard', 'services', 'quotation', 'order', 'my_team', 'subscriptions', 'events', 'notifications', 'ticket'];
 let url = window.location.href;
 
 menuItemsName.forEach((item, index) => {
