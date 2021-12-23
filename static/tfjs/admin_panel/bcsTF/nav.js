8fcf5a68fd670ab47fc53ae1592c5f4df995a44f
salekin-techforing <salekin.techforing@gmail.com>
2021-12-23 18:33:18 +0600

    sidebar preference added

--- static/tfjs/admin_panel/bcsTF/nav.js
+++ static/tfjs/admin_panel/bcsTF/nav.js
@@ -13,7 +13,7 @@ const dropdown = document.querySelectorAll(".dropdown-btn");
 for (let i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function () {
     var dropdownContent = this.nextElementSibling;
-    dropdownContainers.forEach(item=>{
+    dropdownContainers.forEach(item => {
       item.classList.remove("show-dropdown-container")
       if (item.classList.contains("active-container") && item != dropdownContent) {
         item.classList.remove("active-container");
@@ -32,12 +32,12 @@ for (let i = 0; i < dropdown.length; i++) {
   });
 }
 const mainSection = document.querySelector("main")
-mainSection.addEventListener("click", ()=>{
-  dropdownContainers.forEach(item=>{
+mainSection.addEventListener("click", () => {
+  dropdownContainers.forEach(item => {
     item.classList.remove("show-dropdown-container")
     item.classList.remove("active-container")
   })
-  dropdown.forEach(item=>{
+  dropdown.forEach(item => {
     item.classList.remove("clicked");
   })
 })
@@ -67,33 +67,45 @@ hamburger.addEventListener("click", () => {
 sideBarClose.addEventListener("click", () => {
   aside.classList.remove("show-aside")
 })
+let expanded = localStorage.getItem("sidebar-expanded");
+const shrinkBtn = document.querySelector(".shrink-btn");
+let expandBtn = document.querySelector('.menu-expand-btn');
 
-const shrinkBtn = document.querySelector(".shrink-btn")
 shrinkBtn.addEventListener("click", () => {
-  aside.classList.toggle("shrink-container")
-  nav.classList.toggle("nav-expand")
-  main.classList.toggle("main-expand")
-  footer.classList.toggle("footer-expand")
-  sideBarText.forEach(text=>{
-    text.classList.toggle("d-none")
-  })
-
+  expanded = expanded == 'true' ? 'false' : 'true';
+  // Store value to local storage
+  localStorage.setItem("sidebar-expanded", expanded);
+  updateSidebar();
 })
-// aside.addEventListener("mouseover", () => {
-//   if (aside.classList.contains("mouseover"))
-//     aside.classList.remove("shrink-container")
-// })
+expandBtn.addEventListener("click", () => {
+  expanded = expanded == 'true' ? 'false' : 'true';
+  // Store value to local storage
+  localStorage.setItem("sidebar-expanded", expanded);
+  updateSidebar();
+})
 
-// aside.addEventListener("mouseout", () => {
-//   if (aside.classList.contains("mouseover"))
-//     aside.classList.add("shrink-container")
-// })
-
-
-// if(){
-//   console.log("true");
-//   aside.addEventListener("mouseover",()=>{
-//     if(aside.classList.contains("shrink-container"))
-//       aside.classList.remove("shrink-container")
-//   })
-// }
+const updateSidebar = () => {
+  if (expanded == 'true') {
+    aside.classList.remove("shrink-container")
+    nav.classList.remove("nav-expand")
+    main.classList.remove("main-expand")
+    footer.classList.remove("footer-expand")
+    sideBarText.forEach(text => {
+      text.classList.remove("d-none")
+    });
+    expandBtn.classList.remove('d-none')
+    shrinkBtn.classList.add('invisible')
+  }
+  else {
+    aside.classList.add("shrink-container")
+    nav.classList.add("nav-expand")
+    main.classList.add("main-expand")
+    footer.classList.add("footer-expand")
+    sideBarText.forEach(text => {
+      text.classList.add("d-none")
+    });
+    expandBtn.classList.add('d-none')
+    shrinkBtn.classList.remove('invisible')
+  }
+}
+updateSidebar();
