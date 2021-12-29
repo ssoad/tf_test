58215c720bcae01e75c84520684ebfe799691842
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2021-12-29 15:10:21 +0600

    bcs academy

--- Academy/models.py
+++ Academy/models.py
@@ -86,3 +86,54 @@ class CoursePurchase(models.Model):
 
     def __str__(self):
         return f'{self.user.full_name} - {self.course.course_name}'
+
+
+class BCSCourse(models.Model):
+    product_id = models.CharField(max_length=255)
+    course_category = models.ForeignKey(CourseCategory, on_delete=models.CASCADE,
+                                        related_name='bcscourse_coursecategory')
+    # course_type = models.CharField(max_length=264, choices=course_type)
+    course_name = models.CharField(max_length=264)
+    short_description = models.TextField(max_length=1000)
+    long_description = HTMLField(max_length=5000)
+    created_date = models.DateTimeField(auto_now_add=True)
+    edited_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.course_name
+
+
+
+class BCSSection(models.Model):
+    course = models.ForeignKey(BCSCourse, on_delete=models.CASCADE, related_name='bcssection_bcscourse')
+    section_name = models.CharField(max_length=264)
+
+    def __str__(self):
+        return self.section_name
+
+
+class BCSContent(models.Model):
+    section = models.ForeignKey(BCSSection, on_delete=models.CASCADE, related_name='bcscontent_bcssection')
+    lecture_name = models.CharField(max_length=264)
+    text_instruction = models.FileField(upload_to='course/')
+    course_video = models.FileField(upload_to='course/')
+    preview_video = models.FileField(upload_to='course/',
+                                     blank=True, null=True)
+    resource_file = models.FileField(upload_to='course/')
+
+    def __str__(self):
+        return self.lecture_name
+
+    def get_duration(self):
+        filename = self.course_video.url
+
+        cap = cv2.VideoCapture(filename[1:])
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        duration = frame_count / fps
+
+        minutes = int(duration / 60)
+        seconds = int(duration % 60)
+
+        cap.release()
+        return str(minutes) + ':' + str(seconds) + ' m'
