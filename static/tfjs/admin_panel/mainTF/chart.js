d522650236936f0c7cc340e2129392f32f49494a
salekin-techforing <salekin.techforing@gmail.com>
2021-12-30 12:40:02 +0600

    api url bug fixed, removed unnecessary code

--- static/tfjs/admin_panel/mainTF/chart.js
+++ static/tfjs/admin_panel/mainTF/chart.js
@@ -1,59 +1,9 @@
-let userChart = document.getElementById("user-chart").getContext("2d");
 let infoChart = document.getElementById("info-chart").getContext("2d");
-let userLineChart = new Chart(userChart, {
-    type: "line",
-    data: {
-        labels: ["1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"],
-        datasets: [{
-            label: "New User",
-            backgroundColor: "black",
-            borderColor: "black",
-            data: [5, 45, 41, 50, 42],
-            fill: false,
-        },
-        ],
-    },
-    options: {
-        responsive: true,
-        legend: {
-            display: false,
-            labels: {
-                fontColor: "black",
-            },
-            position: "top",
-        },
-        title: {
-            display: true,
-            position: "top",
-            align: 'start',
-            text: "Users site visits in last 5 hours",
-        },
-        tooltips: {
-            mode: "index",
-            intersect: false,
-        },
-        hover: {
-            mode: "nearest",
-            intersect: true,
-        },
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    display: false
-                }
-            }],
-            yAxes: [{
-                gridLines: {
-                    display: false
-                }
-            }]
-        },
-    },
-});
+
 let infoLineChart = new Chart(infoChart, {});
 let getChartData = type => {
 
-    fetch(`http://127.0.0.1:8000/api/main/main_admin_${type}_chart/`)
+    fetch(`${mainOrigin}/api/main/main_admin_${type}_chart/`)
         .then(response => {
             return response.json()
         })
