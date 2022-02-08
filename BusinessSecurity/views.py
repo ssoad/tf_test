14f35aaff4f20f5e6ed75907f158f95c685dbf73
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-02-08 17:38:02 +0600

    cronjob updated for bsc course subscription

--- BusinessSecurity/views.py
+++ BusinessSecurity/views.py
@@ -3071,12 +3071,33 @@ def bcsAdminTicketsDetailView(request, id):
 
 
 def TestView(request):
-    # username = 'AfTmv1E8P0HbJCkRMtm7s_07rqkJCGvp4WufOBxLWUl5AFujlsqmn6WdpMZo-nQr-yKVTnogZOQYgLnl'
-    # password = 'EOsLHpTI748BbKSwcWlQpgmuJZXyudRnJP50Gc8H5Anf8VnDfk8FtEtRYwJ_iU1T9sgH5DOv53BuqeyH'
-    # busername = str(base64.b64encode(bytes(username, 'utf-8')))[1:].replace("'", "").replace("=", '')
-    # bpassword = str(base64.b64encode(bytes(password, 'utf-8')))[1:].replace("'", "")
-    # bearer = f"Basic {busername}6{bpassword}"
+    username = 'AfTmv1E8P0HbJCkRMtm7s_07rqkJCGvp4WufOBxLWUl5AFujlsqmn6WdpMZo-nQr-yKVTnogZOQYgLnl'
+    password = 'EOsLHpTI748BbKSwcWlQpgmuJZXyudRnJP50Gc8H5Anf8VnDfk8FtEtRYwJ_iU1T9sgH5DOv53BuqeyH'
+    busername = str(base64.b64encode(bytes(username, 'utf-8')))[1:].replace("'", "").replace("=", '')
+    bpassword = str(base64.b64encode(bytes(password, 'utf-8')))[1:].replace("'", "")
+    bearer = f"Basic {busername}6{bpassword}"
+
+    # all_orders_subscriptions = CourseOrder.objects.filter(is_active=True)
+    # for subscription in all_orders_subscriptions:
+    #     subscription_id = subscription.payment_id
     #
+    #     url = f'{settings.PAYPAL_URL}billing/subscriptions/{subscription_id}/'
+    #
+    #     headers = {
+    #         'Content-type': 'application/json',
+    #         'Authorization': bearer
+    #     }
+    #     r = requests.get(url, headers=headers)
+    #     if r.json()['status'] != 'ACTIVE':
+    #         subscription.is_active = False
+    #         subscription.save()
+    #
+    #     order_subscription_teams = SubscriptionTeam.objects.filter(subscription_order__payment_id=subscription_id)
+    #
+    #         if order_subscription_teams.exists():
+    #             for team in order_subscription_teams:
+    #                 team.delete()
+
     # all_subscriptions = models.SubscriptionOrder.objects.filter(is_active=True)
     # for subscription in all_subscriptions:
     #     subscription_id = subscription.paypal_subscription_id
@@ -3091,5 +3112,5 @@ def TestView(request):
     #     if r.json()['status'] != 'ACTIVE':
     #         subscription.is_active = False
     #         subscription.save()
-    print(settings.PAYPAL_USER)
+    # print(settings.PAYPAL_USER)
     return HttpResponse('r.content')
