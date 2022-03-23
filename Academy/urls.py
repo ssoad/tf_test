347ff8740f584df56e22f046e02bc56a2ce55111
faruk <faruk.techforing@gmail.com>
2022-03-23 11:33:02 +0600

    Changed in academy url ambigious views corporateTrainingView

--- Academy/urls.py
+++ Academy/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 
     path('academy_faq/', views.academyFAQView, name='academy_faq'),
     path('corporate_training/', views.corporateTrainingView, name='corporate_training'),
-    path('law_enforcement/', views.corporateTrainingView, name='law_enforcement'),
+    path('law_enforcement/', views.lawEnforcementView, name='law_enforcement'),
     path('educational_institute/', views.educationalInstituteView, name='educational_institute'),
 
     path('download/', views.download_file, name='demo_download'),
