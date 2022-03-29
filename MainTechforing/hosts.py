565e27dddaf37ede6989efdb9de2e88e7f89809e
Mofajjal Rasul <mofajjal.techforing@gmail.com>
2022-03-29 19:04:20 +0600

    Revert "Local settings"
    This reverts commit b5b466a2c97cefc88ad47818c6c4c7011994489b.

--- MainTechforing/hosts.py
+++ MainTechforing/hosts.py
@@ -4,20 +4,20 @@ from django_hosts import patterns, host
 host_patterns = patterns(
     '',
     # For Production
-    # host(r'main', settings.ROOT_URLCONF, name='main'), # <-- Change it to host(r'', settings.ROOT_URLCONF, name='')
-    # # <-- The `name` we used to in the `DEFAULT_HOST` setting
-    # # host(r'help', 'help.urls', name='help'),
-    # host(r'training', 'Academy.urls', name='academy'),
-    # host(r'pcs', 'PersonalSecurity.urls', name='mysecurity'),
-    # host(r'main', settings.ROOT_URLCONF, name='blog'), # <-- Change it to host(r'blog', settings.ROOT_URLCONF, name='blog')
-    # host(r'main', settings.ROOT_URLCONF, name='account'),
-    # host(r'main', settings.ROOT_URLCONF, name='api'),
+    host(r'main', settings.ROOT_URLCONF, name='main'), # <-- Change it to host(r'', settings.ROOT_URLCONF, name='')
+    # <-- The `name` we used to in the `DEFAULT_HOST` setting
+    # host(r'help', 'help.urls', name='help'),
+    host(r'training', 'Academy.urls', name='academy'),
+    host(r'pcs', 'PersonalSecurity.urls', name='mysecurity'),
+    host(r'main', settings.ROOT_URLCONF, name='blog'), # <-- Change it to host(r'blog', settings.ROOT_URLCONF, name='blog')
+    host(r'main', settings.ROOT_URLCONF, name='account'),
+    host(r'main', settings.ROOT_URLCONF, name='api'),
 
 
     # For Development
-    host(r'', settings.ROOT_URLCONF, name='main'),
-    host(r'', settings.ROOT_URLCONF, name='academy'),
-    host(r'', settings.ROOT_URLCONF, name='account'),
-    host(r'', settings.ROOT_URLCONF, name='mysecurity'),
-    host(r'', settings.ROOT_URLCONF, name='blog'),
+    # host(r'', settings.ROOT_URLCONF, name='main'),
+    # host(r'', settings.ROOT_URLCONF, name='academy'),
+    # host(r'', settings.ROOT_URLCONF, name='account'),
+    # host(r'', settings.ROOT_URLCONF, name='mysecurity'),
+    # host(r'', settings.ROOT_URLCONF, name='blog'),
 )
