a3317c6e1b22765b636948c459cbb55db38c864f
faruk <faruk.techforing@gmail.com>
2022-04-25 17:53:26 +0600

    changes for production

--- MainTechforing/hosts.py
+++ MainTechforing/hosts.py
@@ -4,12 +4,12 @@ from django_hosts import patterns, host
 host_patterns = patterns(
     '',
     # For Production
-    host(r'main', settings.ROOT_URLCONF, name='main'), # <-- Change it to host(r'', settings.ROOT_URLCONF, name='')
+    host(r'', settings.ROOT_URLCONF, name=''), # <-- Change it to host(r'', settings.ROOT_URLCONF, name='')
     # <-- The `name` we used to in the `DEFAULT_HOST` setting
     # host(r'help', 'help.urls', name='help'),
     host(r'training', 'Academy.urls', name='academy'),
     host(r'pcs', 'PersonalSecurity.urls', name='mysecurity'),
-    host(r'main', settings.ROOT_URLCONF, name='blog'), # <-- Change it to host(r'blog', settings.ROOT_URLCONF, name='blog')
+    host(r'blog', settings.ROOT_URLCONF, name='blog'), # <-- Change it to host(r'blog', settings.ROOT_URLCONF, name='blog')
     host(r'main', settings.ROOT_URLCONF, name='account'),
     host(r'main', settings.ROOT_URLCONF, name='api'),
 
